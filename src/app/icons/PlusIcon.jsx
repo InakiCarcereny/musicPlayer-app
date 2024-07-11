a7086c5a09420a67c22dda2ead7bc912312954c1
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-11 17:05:57 -0300

    add icon

---
+++ src/app/icons/PlusIcon.jsx
@@ -0,0 +1,10 @@
+export function PlusIcon () {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus" width="32" height="32" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 5l0 14" />
+      <path d="M5 12l14 0" />
+    </svg>
+  )
+}
