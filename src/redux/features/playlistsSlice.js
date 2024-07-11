2ec6a23ce61bd87716a9ab864390031453ef7ece
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-11 17:06:15 -0300

    add playlist slice

---
+++ src/redux/features/playlistsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = []
+
+export const playlistsSlice = createSlice({
+  name: 'playlists',
+  initialState,
+  reducers: {
+  }
+})
+
+const {  } = playlistsSlice.actions
+ 
+export default playlistsSlice.reducer
