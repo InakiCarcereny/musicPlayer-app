67a49f4503dd2233c4c78b793e92d0a8b0726905
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-11 17:06:24 -0300

    create filters

---
+++ src/app/components/Filters.jsx
@@ -0,0 +1,18 @@
+export function Filters () {
+
+  return (
+    <div className="flex items-center gap-4">
+      <span className="text-sm text-gray-500 font-semibold bg-gray-100 rounded-lg py-1 px-3 cursor-pointer">
+        Playlitst
+      </span>
+
+      <span className="text-sm text-gray-500 font-semibold bg-gray-100 rounded-lg py-1 px-3 cursor-pointer">
+        Albums
+      </span>
+
+      <span className="text-sm text-gray-500 font-semibold bg-gray-100 rounded-lg py-1 px-3 cursor-pointer">
+        Songs
+      </span>
+    </div>
+  )
+}
