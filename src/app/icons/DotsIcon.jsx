1612df35775869eae03d3393c624f68fd6665e09
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-12 17:00:46 -0300

    add icons

---
+++ src/app/icons/DotsIcon.jsx
@@ -0,0 +1,11 @@
+export function DotsIcon () {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-dots-vertical" width="40" height="40" viewBox="0 0 24 24" stroke="#000000" fill="none">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    </svg>
+  )
+}
