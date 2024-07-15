d7bef9cb8447306b0e8e6378507f9819445755ca
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-15 20:18:44 -0300

    add icons

---
+++ src/app/icons/PlayIcon.jsx
@@ -0,0 +1,9 @@
+export function PlayIcon () {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler text-indigo-500 icon-tabler-player-play-filled" width="32" height="32" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" />
+    </svg>
+  )
+}
