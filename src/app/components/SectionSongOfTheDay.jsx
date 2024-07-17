833d5db40235c60ed5b073767eb39a4536e22bd3
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-17 17:30:00 -0300

    get random song of the day

--- src/app/components/SectionSongOfTheDay.jsx
+++ src/app/components/SectionSongOfTheDay.jsx
@@ -1,9 +1,12 @@
 'use client'
 
-import { useSelector } from "react-redux"
+import { useSongOfTheDay } from "../hooks/useSongOfTheDay"
 
-export function SectionSongOfTheDay ({ albums }) {
-  const album = useSelector(state => state.albums.albums)
+export function SectionSongOfTheDay () {
+
+  const { getRandomSong } = useSongOfTheDay()
+
+  const song = getRandomSong()
 
   return (
     <section className="mt-8">
@@ -12,37 +15,23 @@ export function SectionSongOfTheDay ({ albums }) {
       </h2>
 
       <div className="mt-4">
-        {
-          album.slice(0, 1).map((album) => {
-            return (
-              <div key={album.id}>
-                {
-                  album.songs.slice(0, 1).map((song) => {
-                    return (
-                      <div key={song.id} className="flex flex-col items-center justify-center max-w-full bg-gray-100 rounded-lg pt-4 shadow-md">
-                        <img 
-                        className="w-[400px] h-[200px] rounded-lg"
-                        src={song.cover} 
-                        alt={song.title} />
-                        <div className="flex items-center justify-center gap-2 pt-2">
-                          <span className="font-medium text-indigo-500">
-                            {song.title}
-                          </span>
-                          <span className="text-xs text-gray-500 font-semibold">
-                            ●
-                          </span>
-                          <span className="text-xs text-gray-500 font-semibold">
-                            {song.artists}
-                          </span>
-                        </div>
-                      </div>
-                    )
-                  })
-                }
-              </div>
-            )
-          })
-        }
+        <div key={song.id} className="flex flex-col items-center justify-center max-w-full bg-gray-100 rounded-lg pt-4 shadow-md">
+          <img 
+          className="w-[400px] h-[200px] rounded-lg"
+          src={song.cover} 
+          alt={song.title} />
+          <div className="flex items-center justify-center gap-2 pt-2">
+            <span className="font-medium text-indigo-500">
+              {song.title}
+            </span>
+            <span className="text-xs text-gray-500 font-semibold">
+              ●
+            </span>
+            <span className="text-xs text-gray-500 font-semibold">
+              {song.artists}
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   )
