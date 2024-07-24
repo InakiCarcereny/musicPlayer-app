96b5fcdf91983b3e8462034db2431d5e7ba1cad0
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-24 12:31:07 -0300

    fix change song depending the day

--- src/app/hooks/useSongOfTheDay.js
+++ src/app/hooks/useSongOfTheDay.js
@@ -3,46 +3,15 @@ import { useSelector } from "react-redux"
 export function useSongOfTheDay () {
   const album = useSelector(state => state.albums.albums)
 
-  const date = new Date()
-  const day = date.getDay()
-
-  const randomAlbum = Math.floor(Math.random(album.length))
-
-  const randomSong = Math.floor(Math.random(album[randomAlbum].songs.length))
-
-  //console.log(album[randomAlbum].songs[randomSong])
+  const date = new Date();
+  const day = date.getDay();
 
   const getRandomSong = () => {
-    if(day === 0) {
-      return album[randomAlbum].songs[randomSong]
-    }
+    const randomAlbum = day % album.length;
+    const randomSong = day % album[randomAlbum].songs.length;
 
-    if(day === 1) {
-      return album[randomAlbum].songs[randomSong]
-    }
+    return album[randomAlbum].songs[randomSong];
+  };
 
-    if(day === 2) {
-      return album[randomAlbum].songs[randomSong] 
-    }
-
-    if(day === 3) {
-      return album[randomAlbum].songs[randomSong]
-    }
-
-    if(day === 4) {
-      return album[randomAlbum].songs[randomSong]
-    }
-
-    if(day === 5) {
-      return album[randomAlbum].songs[randomSong]
-    }
-
-    if(day === 6) {
-      return album[randomAlbum].songs[randomSong]
-    }
-
-    return album[randomAlbum].songs[randomSong]
-  }
-
-  return { getRandomSong }
+  return { getRandomSong };
 }
