df7b3d945003ecadb477ac3d92c2fe862133ae60
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-24 15:44:19 -0300

    provide the filter context

--- src/app/routes/favorites/page.jsx
+++ src/app/routes/favorites/page.jsx
@@ -1,12 +1,18 @@
+'use client'
+
 import { HeaderFavorites } from "@/app/components/HeaderFavorites";
 import { SectionFavorites } from "@/app/components/SectionFavorites";
+import { FiltersProvider } from "@/app/context/filters";
 
 export default function Favorites() {
   return (
-    <div className="flex flex-col flex-1">
-      <HeaderFavorites />
 
-      <SectionFavorites />
-    </div>
+    <FiltersProvider>
+      <div className="flex flex-col flex-1">
+        <HeaderFavorites />
+
+        <SectionFavorites />
+      </div>
+    </FiltersProvider>
   )
 }
