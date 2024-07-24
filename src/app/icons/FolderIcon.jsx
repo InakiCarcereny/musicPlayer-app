6995573b395b5ad15ff766cbed5c8541c9aa5cc5
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-24 15:45:08 -0300

    change scale of the icon

--- src/app/icons/FolderIcon.jsx
+++ src/app/icons/FolderIcon.jsx
@@ -1,7 +1,7 @@
 export function FolderIcon () {
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-folder-minus" width="40" height="40" viewBox="0 0 24 24" stroke="#000000" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-folder-minus" width="48" height="48" viewBox="0 0 24 24" stroke="#000000" fill="none">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
       <path d="M12 19h-7a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v6" />
       <path d="M16 19h6" />
