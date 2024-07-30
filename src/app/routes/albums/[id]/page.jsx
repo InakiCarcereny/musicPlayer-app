d492ea6933eec952e130e0a0e285a27fa72d80d7
inakicarcereny <inakicarce@hotmail.com.ar>
2024-07-30 16:58:16 -0300

    render the preview of the album

--- src/app/routes/albums/[id]/page.jsx
+++ src/app/routes/albums/[id]/page.jsx
@@ -6,38 +6,60 @@ import { useSelector } from "react-redux"
 export default function Albums({ params }) {
   const album = useSelector(state => state.albums.albums)
   const favorites = useSelector(state => state.favorites)
-  
-  //console.log(album)
 
   const ID = params.id
 
   const albumId = parseInt(ID)
 
-  //console.log(albumId)
+  const albumFilter = album.filter(album => album.id === albumId)
 
-  const albumData = album.find(album => album.id === albumId)
+  const songData = albumFilter.find(song => song.id === albumId)
 
   const checkFavorite = (song) => {
-    //console.log(song)
     return favorites.some(favorite => favorite.song.number === song.id)
   }
 
-  //console.log(albumData)
+  //console.log(favorites)
 
   return (
-    <ul className="flex flex-col gap-2 mt-8">
+    <ul className="flex flex-col gap-4 mt-8">
+
       {
-        albumData.songs.map((song) => {
+        albumFilter.map((album) => {
           return (
-            <Song 
-            key={song.id} 
-            title={song.title} 
-            artists={song.artists} 
-            cover={song.cover}
-            number={song.id}
-            isFavorite={checkFavorite(song)}
-            albumId={albumId}
-            />
+            <>
+            <div key={album.id} className="flex items-center gap-4">
+              <img src={album.cover} 
+              alt={album.name}
+              className="w-[200px] h-[200px] rounded-lg shadow-xl"
+              />
+
+              <div className="flex flex-col gap-2">
+                <p className="font-base text-sm text-gray-500">{album.subtitle}</p>
+                <h2 className="font-bold text-4xl text-indigo-500">{album.name}</h2>
+                <span className="text-sm font-semibold text-gray-500">{album.artist}</span>
+              </div>
+            </div>
+            
+            <div className="flex flex-col gap-2">
+              {
+                songData.songs.map((song, index) => {
+                  return (
+                    <Song 
+                    key={song.id} 
+                    title={song.title} 
+                    artists={song.artists} 
+                    cover={song.cover}
+                    number={song.id}
+                    isFavorite={checkFavorite(song)}
+                    albumId={albumId}
+                    index={index}
+                    />
+                  )
+                })
+              }
+            </div>
+            </>
           )
         })
       }
