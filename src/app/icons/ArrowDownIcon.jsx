a725c5349940f1aa5ba8892d8c6484ed799a35dc
inakicarcereny <inakicarce@hotmail.com.ar>
2024-08-01 10:45:48 -0300

    add icons

---
+++ src/app/icons/ArrowDownIcon.jsx
@@ -0,0 +1,9 @@
+export function ArrowDownIcon () {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevron-down" width="36" height="36" viewBox="0 0 24 24" stroke="#9e9e9e" fill="none">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M6 9l6 6l6 -6" />
+    </svg>
+  )
+}
