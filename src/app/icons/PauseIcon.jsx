9ca26eaef1e147f01a714b61ac4279e575560b93
inakicarcereny <inakicarce@hotmail.com.ar>
2024-08-05 14:07:09 -0300

    change the icons on the modal player

--- src/app/icons/PauseIcon.jsx
+++ src/app/icons/PauseIcon.jsx
@@ -1,7 +1,7 @@
-export function PauseIcon () {
+export function PauseIcon ({ className }) {
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler text-indigo-500 icon-tabler-player-pause-filled" width="32" height="32" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" className={`${className} text-indigo-500`} width="32" height="32" viewBox="0 0 24 24" stroke="currentColor" fill="none">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
       <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
       <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
--- src/app/icons/PlayIcon.jsx
+++ src/app/icons/PlayIcon.jsx
@@ -1,7 +1,7 @@
-export function PlayIcon () {
+export function PlayIcon ({ className }) {
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler text-indigo-500 icon-tabler-player-play-filled" width="32" height="32" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" class={`${className} text-indigo-500`} width="32" height="32" viewBox="0 0 24 24" stroke="currentColor" fill="none">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
       <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" />
     </svg>
